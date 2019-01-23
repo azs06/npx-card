9dc88a9fe1a453dcf8f524ab673efcfdc5c0b90e
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2019-01-23 18:57:07 +0600

    updated card information

--- build.js
+++ build.js
@@ -15,23 +15,21 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('               Md. Atiquzzaman Soikat'),
+  handle: chalk.white('azs06'),
+  work: chalk.white('Software Engineer at ButlerHospitality'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('soikat'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~azs06'),
+  github: chalk.gray('https://github.com/') + chalk.green('azs06'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('md-atiquzzaman-soikat'),
+  stackoverflow: chalk.gray('https://stackoverflow.com/users/') + chalk.blue('946131/azs06'),
+  npx: chalk.red('npx') + ' ' + chalk.white('azs06'),
   labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
+  labelStackOverflow: chalk.white.bold('   StackOverflow:'),
   labelCard: chalk.white.bold('       Card:')
 }
 
@@ -39,24 +37,22 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
+const stackoverflowing = `${data.labelStackOverflow} ${data.stackoverflow}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
                twittering + newline + // data.labelTwitter + data.twitter
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
+               stackoverflowing + newline + 
                carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
